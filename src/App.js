3a3c1499d6d15c9b738319880e5838e6245b2fa8
drnieves <ernestonievesurosario@gmail.com>
2021-10-26 20:18:25 -0500

    header completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Particles from "react-particles-js";
+
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            color: {
+              value: "#3ca6b1",
+            },
+            links: {
+              color: "#e53278",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            shape: {
+              type: "circle",
+            },
+            number: {
+              value: 30,
+            },
+            size: {
+              value: 3,
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
